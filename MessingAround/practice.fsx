56681319064264ca43a403de5512ee3f6d0c1396
Jmille65 <jordanmiller@web106033.wireless.mcgill.ca>
2017-02-22 21:55:03 -0500

    more higher-order function practice

--- MessingAround/practice.fsx
+++ MessingAround/practice.fsx
@@ -28,3 +28,52 @@ let result = // evals to 7- env diagram practice
     let x = x + 1 in
     fun y -> x+y
   f x
+
+// attempt 1
+//let bits n =
+//  let rec helper n list =
+//    match n with
+//      | n when n = 1 -> list
+//      | n when n > 1 ->
+//          let x::xs = list
+//          let y = 0::x
+//          let z = 1::x
+//          helper (n-1) (y::z::(helper n xs))
+//      | n when n < 1 -> failwith "invalid number"
+//  helper  n [[0];[1]]
+
+//attempt 2
+//let bits n =
+//  let rec helper n list =
+//    match n with
+//      | 0 -> [[]]
+//      | n when n > 0 ->
+//          helper (n-1) (List.fold (fun (x::xs) list -> (0::x)::(1::x)::list) list list)
+//  helper n []
+
+//code from someone smarter than me
+let rec bits n =
+  match n with
+    | 0 -> [[]]
+    | x ->
+      let reList = bits(n-1)
+      (List.map (fun u -> 0::u) reList)@(List.map (fun y -> 1::y) reList)
+let a = bits 3
+
+let tripleMinusOne f =
+  fun x -> ((f (f (f x)))-1)
+
+let multByTwo n =
+  n * 2
+
+let dMBT = triple multByTwo
+let b = dMBT 2
+
+let rec repeat f n =
+  if n = 0 then
+    fun x -> x
+  else
+    fun x -> f(repeat f (n-1) x)
+
+let rMBT = repeat multByTwo 3
+let c = rMBT 2
